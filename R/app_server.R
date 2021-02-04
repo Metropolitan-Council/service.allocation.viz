8d9f9af313228a02582555376e7da151b6b589ed
Liz Roten <liz.roten@metc.state.mn.us>
2021-02-04 16:18:08 -0600

    add slider and table server elements

--- R/app_server.R
+++ R/app_server.R
@@ -13,13 +13,19 @@ app_server <- function(input, output, session) {
     "scrolly_container_ui_1"
   )
 
+  slider_input <- callModule(mod_slider_server, "slider_ui_1")
+
+
 
   callModule(mod_plot_server, "plot_ui_1",
     scroll_section = scrolly_section
   )
-
   callModule(mod_plot_server, "plot_ui_2",
              scroll_section = scrolly_section
   )
+
+
+  callModule(mod_table_server, "table_ui_1",
+             slider_input = slider_input)
 
 }
