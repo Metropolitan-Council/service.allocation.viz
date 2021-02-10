5c12e0c9eecf9a190bf13117c55d00ce64f66b91
Liz Roten <liz.roten@metc.state.mn.us>
2021-02-10 16:59:00 -0600

    format plotly some

--- R/mod_plot_scenario_summary.R
+++ R/mod_plot_scenario_summary.R
@@ -33,30 +33,52 @@ mod_plot_scenario_summary_server <- function(input, output, session,
 
   output$scenario_summary <- plotly::renderPlotly({
     ggplotly(
+      tooltip = "label",
       ggplot(data = summary_data()) +
-        geom_tile(aes(x = c(1,0,1,0),
-                      y = 1,
-                      fill = item_category),
-                  show.legend = F) +
+        geom_tile(aes(
+          x = c(1, 0, 1, 0),
+          y = 1,
+          fill = item_category
+        ),
+        show.legend = F
+        ) +
         facet_wrap(~expand_improve,
-                   labeller = labeller(expand_improve = c(Expand = "Expand Access",
-                                                          Improve = "Improved Transit Service"))) +
-        geom_text(aes(x = c(1,0,1,0),
-                      y = 1,
-                      label = stringr::str_wrap(lab, width = 9)),
-                  position = position_dodge(width = 0)) +
-        scale_fill_manual(labels = c("Jobs",
-                                     "People"),
-                          values = c("#E2F0D9",
-                                     "#DAE3F3")) +
-        theme(axis.text.x = element_blank(),
-              axis.text.y = element_blank(),
-              axis.title.x = element_blank(),
-              axis.title.y = element_blank())
-    )
-
+          labeller = labeller(expand_improve = c(
+            Expand = "Expand Access",
+            Improve = "Improved Transit Service"
+          ))
+        ) +
+        geom_text(aes(
+          x = c(1, 0, 1, 0),
+          y = 1,
+          label = stringr::str_wrap(lab, width = 9)
+        ),
+        position = position_dodge(width = 0)
+        ) +
+        scale_fill_manual(
+          labels = c(
+            "Jobs",
+            "People"
+          ),
+          values = c(
+            "#E2F0D9",
+            "#DAE3F3"
+          )
+        ) +
+        councilR::council_theme(use_showtext = TRUE) +
+        theme(
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank()
+        )
+    ) %>%
+      plotly::config(
+        displaylogo = F,
+        showSendToCloud = F,
+        displayModeBar = F
+      )
   })
-
 }
 
 ## To be copied in the UI
