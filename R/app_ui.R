db70669d3b21faa6ec5fa9b2cf85527ae594090d
Liz Roten <liz.roten@metc.state.mn.us>
2021-04-29 15:11:02 -0500

    use favicon

--- R/app_ui.R
+++ R/app_ui.R
@@ -92,7 +92,7 @@ golem_add_external_resources <- function() {
   suppressDependencies()
 
   tags$head(
-    # favicon(),
+    favicon(),
     bundle_resources(
       path = app_sys("app/www"),
       app_title = "Service Allocation Study"
